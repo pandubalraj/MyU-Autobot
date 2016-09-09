84f40ee29dbece30b94d28644768707c3cdc2ad9
pandubalraj <pandubalraj@gmail.com>
2016-09-09 17:36:36 +0530

    change in port

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.get(/.*/, restify.serveStatic({
 	'directory': '.',
 	'default': 'index.html'
 }));
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port|| process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
