0f3c8a0a18af34e86c495335e4d0f0e3328e0ef2
pandubalraj <pandubalraj@gmail.com>
2016-09-09 16:37:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var builder = require('botbuilder');
 //var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v1/application?id=3441c805-65b1-4cc0-8b5e-e6c92b747ca8&subscription-key=c9ad898006c6426d95251f015167aaa1&q=');
 //var dialog  = new builder.IntentDialog({ recognizers: [recognizer] });
 
+// Get secrets from server environment
+var connector = new builder.ChatConnector({
+    appId: '17b4a6fa-5d57-4183-b486-150fe8e36f22', 
+    appSecret: 'ByWDWjABFMgHe8uORjtQ6Pa' 
+});
+
 // Setup Restify Server
 var server = restify.createServer();
 // Handle Bot Framework messages
@@ -18,11 +24,6 @@ server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
-// Get secrets from server environment
-var connector = new builder.ChatConnector({
-    appId: '17b4a6fa-5d57-4183-b486-150fe8e36f22', 
-    appSecret: 'ByWDWjABFMgHe8uORjtQ6Pa' 
-});
 // Create bot
 var bot = new builder.UniversalBot(connector);
 bot.dialog('/', function (session) {
