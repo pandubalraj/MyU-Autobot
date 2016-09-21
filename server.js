6a98535ab972dde361d144bfdd9e2c3674f61e41
Pandu <pandubalraj@gmail.com>
2016-09-21 05:08:00 +0000

    change in server for image with conv -7

--- server.js
+++ server.js
@@ -92,24 +92,15 @@ bot.dialog('/getRegNo', [
     }
 ]);
 
-bot.dialog('/picture', [
-    function (session) {
-        var msg = new builder.Message(session)
-            .attachments([{
-                contentType: "image/jpeg",
-                contentUrl: "http://www.team-bhp.com/forum/attachments/indian-car-scene/164039d1248451018t-high-security-registration-plates-hsrp-india-dsc_6975.jpg"
-            }]);
-        session.endDialog(msg);
-    }
-]);
-
 bot.dialog('/getClaim', [
     function (session) {
         builder.Prompts.confirm(session,'Did you do any claim last year.');
     },    
     function (session, results) {
+        session.send(results.response);
         // session.send('Good you have not claimed till now.')
-        session.send('Find your details here \n Car Model: %s',carModel);
+        session.send('Find your details here');
+        session.send('\n Car Model: %s',carModel);
         session.send('\n Car Cost: %s',carCost);
         session.send('\n Car RegNo: %s',carRegNo);
         session.endDialog();
